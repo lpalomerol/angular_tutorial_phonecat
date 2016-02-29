3d7d617a8e546e9b6f5c8e9bae8b8478c3da0cba
lpalomerol <luis.palomero.lopez.armentia@gmail.com>
2016-02-29 14:07:53 +0100

    Add search filter

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -2,15 +2,35 @@
 
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 
-describe('PhoneListController', function() {
+describe('PhoneCatApp', function() {
+  describe('Pohe list view', function(){
+    
+    beforeEach(function() {
+      browser.get('app/index.html');
+    });
 
-  beforeEach(function() {
-    browser.get('app/index.html');
+    it('should filter the phone list as a user types into the search box', function() {
+
+      var phoneList = element.all(by.repeater('phone in phones'));
+      var query = element(by.model('query'));
+
+      expect(phoneList.count()).toBe(3);
+
+      query.sendKeys('nexus');
+      expect(phoneList.count()).toBe(1);
+
+      query.clear();
+      query.sendKeys('motorola');
+      expect(phoneList.count()).toBe(2);
+    });
   });
 
-  it('Should create "phones" model with 3 phontes', function(){
-	  var scope = {};
-	  var ctrl = new PhoneListCtrl(scope);
-	  expect(scope.phones.length).toBe(3);
+  it('should display the current filter value in the title bar', function() {
+    var query = element(by.model('query'));     
+    query.clear();
+    expect(browser.getTitle()).toMatch(/My phone gallery:\s*$/);
+
+    query.sendKeys('nexus');
+    expect(browser.getTitle()).toMatch(/My phone gallery: nexus$/);
   });
 });
