a8d3fa4cbf7745fcd0415d64db2872acd626ec0a
lpalomerol <luis.palomero.lopez.armentia@gmail.com>
2016-02-29 14:42:35 +0100

    Order seacrh results

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -3,34 +3,59 @@
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 
 describe('PhoneCatApp', function() {
-  describe('Pohe list view', function(){
-    
+  describe('Phone list view', function() {
+
     beforeEach(function() {
       browser.get('app/index.html');
     });
 
-    it('should filter the phone list as a user types into the search box', function() {
+    it('should filter the phone list as a user types into the search box',
+        function() {
 
-      var phoneList = element.all(by.repeater('phone in phones'));
+          var phoneList = element.all(by.repeater('phone in phones'));
+          var query = element(by.model('query'));
+
+          expect(phoneList.count()).toBe(3);
+
+          query.sendKeys('nexus');
+          expect(phoneList.count()).toBe(1);
+
+          query.clear();
+          query.sendKeys('motorola');
+          expect(phoneList.count()).toBe(2);
+        });
+
+    it('should display the current filter value in the title bar', function() {
       var query = element(by.model('query'));
-
-      expect(phoneList.count()).toBe(3);
+      query.clear();
+      expect(browser.getTitle()).toMatch(/My phone gallery:\s*$/);
 
       query.sendKeys('nexus');
-      expect(phoneList.count()).toBe(1);
-
-      query.clear();
-      query.sendKeys('motorola');
-      expect(phoneList.count()).toBe(2);
+      expect(browser.getTitle()).toMatch(/My phone gallery: nexus$/);
     });
-  });
 
-  it('should display the current filter value in the title bar', function() {
-    var query = element(by.model('query'));     
-    query.clear();
-    expect(browser.getTitle()).toMatch(/My phone gallery:\s*$/);
+    it('Should be possible to control phone order via the dropdown select box',
+        function() {
+          var phoneNameColumn = element.all(by.repeater('phone in phones')
+              .column('phone.name'));
 
-    query.sendKeys('nexus');
-    expect(browser.getTitle()).toMatch(/My phone gallery: nexus$/);
+          var query = element(by.model('query'));
+
+          function getNames() {
+
+            return phoneNameColumn.map(function(elm) {
+              return elm.getText();
+            });
+          }
+          query.sendKeys('tablet');
+          expect(getNames()).toEqual(
+              [ "Motorola XOOM\u2122 with Wi-Fi", "MOTOROLA XOOM\u2122" ]);
+                 
+           element(by.model('orderProp')).element(by.css('option[value="name"]')).click();
+           expect(getNames()).toEqual([
+           "MOTOROLA XOOM\u2122",
+           "Motorola XOOM\u2122 with Wi-Fi"
+           ]);
+        });
   });
 });
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,18 +1,27 @@
 'use strict';
 
 /* jasmine specs for controllers go here */
+describe('PhoneCat controllers', function(){
+  
+  describe('PhoneListController tests', function() {
+    var scope, ctrl;
+    beforeEach(module('phonecatApp'));
 
-describe('PhoneListController tests', function() {
+    beforeEach(inject(function($controller) {
+      scope = {};
+      ctrl = $controller('PhoneListCtrl', {
+        $scope : scope
+      })
+    }));
 
-	beforeEach(module('phonecatApp'));
+    it('should create "phones" model with 3 phones', function() {
 
-	it('should create "phones" model with 3 phones', inject(function($controller) {
-		var scope = {};
-		var ctrl = $controller('PhoneListCtrl', {
-			$scope : scope
-		});
+      expect(scope.phones.length).toBe(3);
+    });
+    it('should set the default value of orderProp model', function() {
+      expect(scope.orderProp).toBe('age');
+    });
+  });
 
-		expect(scope.phones.length).toBe(3);
-	}));
-
+  
 });
