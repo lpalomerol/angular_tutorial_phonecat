1617f0bd591cd063c8e1e681120bad277dd33aa4
lpalomerol <luis.palomero.lopez.armentia@gmail.com>
2016-02-29 15:52:37 +0100

    Add basic filter

--- test/unit/filtersSpec.js
+++ test/unit/filtersSpec.js
@@ -3,5 +3,14 @@
 /* jasmine specs for filters go here */
 
 describe('filter', function() {
-
+    beforeEach(module('phonecatFilters'));
+    
+    describe('checkmark', function(){
+      it('sould convert boolean values to unicodes',
+          inject(function(checkmarkFilter){
+            expect(checkmarkFilter(true)).toBe('\u2713');
+            expect(checkmarkFilter(false)).toBe('\u2718');
+          }));
+    });
 });
+          
